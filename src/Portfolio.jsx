01ebb141d8372a82a36190d7d7d7c91f890e6603
ireshdilsh <ireshdilshan46@gmail.com>
2025-01-21 17:48:05 +0530

    Remove commented-out icon elements from Portfolio component

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -78,3 +78,11 @@ export default function Portfolio() {
         </div>
     )
 }
+
+
+// <div className="icons">
+//                     <i class="bi bi-facebook"></i>
+//                     <i class="bi bi-instagram"></i>
+//                     <i class="bi bi-twitter"></i>
+//                     <i class="bi bi-linkedin"></i>
+//                     </div>
