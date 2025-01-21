56a8f1bf57fd54db77add108217fd4e00dc89a4e
ireshdilsh <ireshdilshan46@gmail.com>
2025-01-21 13:38:40 +0530

    Update portfolio layout and styles; change favicon and title, add new dependencies

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,7 +1,55 @@
 import 'react'
+import Typed from 'typed.js';
 import "./styles/portfolio.css"
+import logo from "./assets/logo.png"
+
 export default function Portfolio() {
+
+    
     return (
-        <div>Portfolio</div>
+        <div>
+            <nav>
+                <div className="logo-name">
+                    <p>.techProg</p>
+                </div>
+                <div className="links">
+                    <a href="#portfolio">Home</a>
+                    <a href="">About me</a>
+                    <a href="">My Expertise</a>
+                    <a href="">My Showcases</a>
+                    <a href="">Get in Touch</a>
+                </div>
+                <div className="hire-btn">
+                    <button className="btn btn-warning" style={{
+                        backgroundColor: "#ffb742",
+                        color:"#333",
+                        fontSize: "14px",
+                        fontWeight: "500",
+                        height: "45px",
+                        width:"120px",
+                    }}>Hire Me</button>
+                </div>
+            </nav>
+
+            <section id="portfolio">
+               <div className="title">
+                   <p>Welcome to <span>.techProg</span></p>
+                   <hr/>
+               </div>
+                <div className="sub-title">
+                    <p>Hi! Im a <span>Software Devoloper.</span></p>
+                </div>
+                <div className="discription">
+                    <p>I’m a dedicated Software Engineer committed to building scalable and efficient software solutions.</p>
+                </div>
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="opacity-text">
+                    <p>Hello, There !</p>
+                </div>
+            </section>
+
+        </div>
     )
 }
